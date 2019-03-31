b4589819cd4e9fa17be74faaf34e24eceb277864
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-31 23:06:29 +0300

    htto class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './component/profile/profile.component';
+import { FormComponent } from './form/form.component';
+import { ReposComponent } from './component/repos/repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    FormComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule,
